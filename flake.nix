c4a0e20243992e8f70e4df2157ff131138622c40
Adam Gaia <git@agaia.dev>
2022-08-22 19:47:42 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Pretty Print";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        package-name = "pretty-print";
+        run-dependencies = with pkgs; [];
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.shellcheck
+            pkgs.shfmt
+          ];
+        };
+
+        defaultPackage = with import nixpkgs {inherit system; };
+        stdenv.mkDerivation {
+          name = package-name;
+          src = self;
+          installPhase = "mkdir -p $out/lib; install -t $out/lib $name";
+        };
+      });
+}
